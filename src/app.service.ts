f788141694a7637044a0e149d0d76669935af458
nkondojr <105057649+nkondojr@users.noreply.github.com>
2024-09-03 17:16:36 +0300

    Changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Rinward Nkondo!';
+    return 'Hellow World!!!!! Welcome to Rinward Project(Nestjs)';
   }
 }
